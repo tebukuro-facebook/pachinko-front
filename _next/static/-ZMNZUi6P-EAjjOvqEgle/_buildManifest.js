09b27a8c5006b0d48a632c1f0a3e6127130bee46
tebukuro-facebook <tebukuro-facebook@users.noreply.github.com>
2021-07-23 05:35:32 +0000

    deploy: 8db3b1e633882230ae4ab764b6c45ba9ca6ef798

---
+++ _next/static/-ZMNZUi6P-EAjjOvqEgle/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/163-57d50e18fbe217fe1b39.js","static/chunks/pages/index-9b032876638d726a6590.js"],"/_error":["static/chunks/pages/_error-8842bdd603648886795b.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
